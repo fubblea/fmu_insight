a07cb5f39645872666fb80a7ac7c5b3775e5a5c6
fubblea <ajay.arthur@gmail.com>
2025-05-21 15:09:20 -0400

    Created params editor layout

--- src/app/components/param_editor.py
+++ src/app/components/param_editor.py
@@ -1,4 +1,5 @@
 from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
 
 from app.state import AppState
 
@@ -8,7 +9,18 @@ class ParamEditor(QtWidgets.QWidget):
         super().__init__(parent)
         self._state = state
 
-        self.form = QtWidgets.QFormLayout(self)
+        self._layout = QtWidgets.QHBoxLayout(self)
+
+        self._left_box = QtWidgets.QGroupBox("Active Params")
+        self._right_box = QtWidgets.QGroupBox("Params Editor")
+
+        self.active_params = QtWidgets.QTreeWidget()
+        self.active_params.setHeaderHidden(True)
+
+        self._right_layout = QtWidgets.QHBoxLayout(self._left_box)
+        self._right_layout.addWidget(self.active_params)
+
+        self.form = QtWidgets.QFormLayout(self._right_box)
         self.name_edit = QtWidgets.QLineEdit()
         self.name_edit.setReadOnly(True)
         self.unit_edit = QtWidgets.QLineEdit()
@@ -26,7 +38,17 @@ class ParamEditor(QtWidgets.QWidget):
         self.form.addRow("Fixed value:", self.value_edit)
         self.form.addRow("Range:", self._hbox(self.min_edit, self.max_edit))
         self.form.addRow("Distribution:", self.dist_combo)
-        # TODO: populate/commit edits according to selection in model explorer
+
+        self._splitter = QtWidgets.QSplitter(Qt.Orientation.Horizontal)
+        self._splitter.addWidget(self._left_box)
+        self._splitter.addWidget(self._right_box)
+        self._splitter.setStretchFactor(3, 0)
+        self._splitter.setSizes([100, 250])
+        self._splitter.setChildrenCollapsible(False)
+
+        self._layout.addWidget(self._splitter)
+
+        self.setLayout(self._layout)
 
     @staticmethod
     def _hbox(*widgets: QtWidgets.QWidget) -> QtWidgets.QWidget:
